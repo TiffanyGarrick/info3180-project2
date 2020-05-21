aa287bceaecd464d622b3702aedbd6b4ac21cd2b
Ubuntu <ubuntu@ip-172-31-41-248.ec2.internal>
2020-05-21 03:35:49 +0000

    Final Commit

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 UPLOAD_FOLDER = './app/static/uploads'
 # needed for session security, the flash() method in this case stores the message
 # in a session
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/project_two'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zucadmulpitopj:c5e27fea902acececc52fe17d9e58c41185575295facc2e188f5792827939f86@ec2-3-229-210-93.compute-1.amazonaws.com:5432/d7pd4rkb33pr9m'
+#ORIGINAL SQLALCHEMY_DATABASE_URI: 'postgresql://postgres:123@localhost/project_two'
 #postgresql://postgres:@password@localhost/project2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
 app.config['SECRET_KEY'] = '12345@idklO0L' #ORIGINAL KEY THAT EXPIRED: 'Sup3r$3cretkey'
